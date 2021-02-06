86e675ebe19825f2b21c5a58feef312549466551
Michiel Borkent <michielborkent@gmail.com>
2021-02-06 14:12:46 +0100

    alpha 4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject babashka/fs "0.0.1-alpha.3"
+(defproject babashka/fs "0.0.1-alpha.4"
   :description "Babashka file system utilities."
   :url "https://github.com/babashka/fs"
   :scm {:name "git"
