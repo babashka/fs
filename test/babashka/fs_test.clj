8c5cf149da84d2ad07c88d5939b7eac856b9c2bf
Michiel Borkent <michielborkent@gmail.com>
2023-05-24 11:06:48 +0200

    test

--- test/babashka/fs_test.clj
+++ test/babashka/fs_test.clj
@@ -716,7 +716,7 @@
           owner-part (str (last (fs/components path)))]
       (is (= (str (fs/file-owner path)) owner-part))))
   (testing "works for files as well"
-    (let [file (doto (fs/create-temp-file {:path (fs/home)})
+    (let [dir (doto (fs/create-temp-dir)
                  fs/delete-on-exit)
-          owner-part (str (second (fs/components file)))]
-      (is (= (str (fs/file-owner file)) owner-part)))))
+          file-in-dir (fs/create-temp-file {:path dir})]
+      (is (= (str (fs/file-owner dir)) (str (fs/file-owner file-in-dir)))))))
