aed36f10bda703018a30f8971ebe2c1c3db76726
Michiel Borkent <michielborkent@gmail.com>
2024-01-09 12:00:53 +0100

    max-depth

--- test/babashka/fs_test.clj
+++ test/babashka/fs_test.clj
@@ -123,9 +123,9 @@
         (is (= 2 (count (fs/glob tmp-dir1 "foo/bar/baz/*.clj")))))))
   (testing "return also directories"
     (let [tmp-dir1 (temp-dir)
-          nested-dir (fs/file tmp-dir1 "foo" "bar" "baz")
+          nested-dir (fs/file tmp-dir1 "foo")
           _ (fs/create-dirs nested-dir)]
-      (is (= 2 (count (map fs/directory? (fs/glob tmp-dir1 "**/{bar,baz}" {:recursive true}))))))))
+      (is (= 1 (count (map fs/directory? (fs/glob tmp-dir1 "*" {:max-depth 1}))))))))
 
 (deftest create-dir-test
   (is (fs/create-dir (fs/path (temp-dir) "foo"))))
