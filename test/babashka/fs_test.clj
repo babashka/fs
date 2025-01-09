ec81b2c8eff94c8845118ad01244fa2b43949e55
Paolo Holinski <paolo@nextjournal.com>
2025-01-09 20:53:51 +0100

    Proper test

--- test/babashka/fs_test.clj
+++ test/babashka/fs_test.clj
@@ -836,9 +836,7 @@
       (is (= (str (fs/owner dir)) (str (fs/owner file-in-dir)))))))
 
 (deftest issue-135-test
-  (def u (java.net.URI/create (str "jar:file:" (fs/unixify (fs/cwd)) "/test-resources/bencode-1.1.0.jar")))
-  (def fs (java.nio.file.FileSystems/newFileSystem u {}))
-
-  (def path-in-zip (.getPath ^java.nio.file.FileSystem fs "/bencode" (into-array String [])))
-
-  (fs/path path-in-zip "core.clj"))
+  (let [uri (java.net.URI/create (str "jar:file:" (fs/unixify (fs/cwd)) "/test-resources/bencode-1.1.0.jar"))
+        fs (java.nio.file.FileSystems/newFileSystem uri {})
+        path-in-zip (.getPath ^java.nio.file.FileSystem fs "/bencode" (into-array String []))]
+    (is (fs/path path-in-zip "core.clj"))))
