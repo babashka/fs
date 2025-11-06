5d6c9e5f6fb9c24788f1e027f8712c8fb0dc7def
lread <lee@dlread.com>
2025-11-06 12:51:52 -0500

    self review: follow style guide for test names
    Add -test suffix to tests.

--- test/babashka/fs_linter_test.clj
+++ test/babashka/fs_linter_test.clj
@@ -20,12 +20,12 @@
     (clj-kondo/run! {:lint ["src"]})
     (f)))
 
-(deftest with-temp-dir-no-args-found-by-kondo
+(deftest with-temp-dir-no-args-found-by-kondo-test
   (is (match? [{:type :invalid-arity
                 :message #"called with 0 args .*expects 1 or more"}]
               (lint "(fs/with-temp-dir)"))))
 
-(deftest with-temp-dir-first-arg-not-vector-handled-by-hook
+(deftest with-temp-dir-first-arg-not-vector-handled-by-hook-test
   (is (match? [{:type :babashka-fs/with-temp-dir-first-arg-not-vector
                 :level :error
                 :row 3 :col 3
@@ -36,7 +36,7 @@
                     ;  ^^^^^^^^^
                     "  (+ 1 2 3))"))))
 
-(deftest with-temp-dir-vector-arg-empty-handled-by-hook
+(deftest with-temp-dir-vector-arg-empty-handled-by-hook-test
   (is (match? [{:type :babashka-fs/with-temp-dir-vector-arg-needs-binding-symbol
                 :level :error
                 :row 2 :col 19
@@ -47,7 +47,7 @@
                ;                  ^^
                "(fs/with-temp-dir [])"))))
 
-(deftest with-temp-dir-vector-arg-binding-name-not-symbol-handled-by-hook
+(deftest with-temp-dir-vector-arg-binding-name-not-symbol-handled-by-hook-test
   (is (match? [{:type :babashka-fs/with-temp-dir-vector-arg-needs-binding-symbol
                 :level :error
                 :row 2 :col 21
@@ -58,7 +58,7 @@
                ;                    ^^^^^
                "(fs/with-temp-dir [ :oops ])"))))
 
-(deftest with-temp-dir-extra-value-in-vector-handled-by-hook
+(deftest with-temp-dir-extra-value-in-vector-handled-by-hook-test
   (is (match? [{:type :babashka-fs/with-temp-dir-vector-arg-extra-value
                 :level :error
                 :row 2 :col 29
@@ -70,7 +70,7 @@
                "(fs/with-temp-dir [bname {} 'extra]"
                "  (spit bname \"foo\"))"))))
 
-(deftest with-temp-dir-extra-values-in-vector-handled-by-hook
+(deftest with-temp-dir-extra-values-in-vector-handled-by-hook-test
   (is (match? [{:type :babashka-fs/with-temp-dir-vector-arg-extra-value
                 :level :error
                 :row 2 :col 29
@@ -87,33 +87,33 @@
                "(fs/with-temp-dir [bname {} 'extra1 'extra2]"
                "  (spit bname \"foo\"))"))))
 
-(deftest with-temp-dir-unused-binding-found-by-kondo
+(deftest with-temp-dir-unused-binding-found-by-kondo-test
   (is (match? [{:type :unused-binding}]
               (lint "(fs/with-temp-dir [unused-binding])"))))
 
-(deftest with-temp-dir-binding-name-with-no-options-is-fine
+(deftest with-temp-dir-binding-name-with-no-options-is-fine-test
   (is (match? []
               (lint "(fs/with-temp-dir [bname]"
                     "  (spit bname \"foo\"))"))))
 
-(deftest with-temp-dir-binding-name-with-options-is-fine
+(deftest with-temp-dir-binding-name-with-options-is-fine-test
   (is (match? []
               (lint "(fs/with-temp-dir [bname {}]"
                     "  (spit bname \"foo\"))"))))
 
-(deftest with-temp-dir-options-from-binding-is-fine
+(deftest with-temp-dir-options-from-binding-is-fine-test
   (is (match? []
               (lint "(let [some-opts {}]"
                     "  (fs/with-temp-dir [bname some-opts]"
                     "    (spit bname \"foo\")))"))))
 
-(deftest with-temp-dir-options-from-fn-call-is-fine
+(deftest with-temp-dir-options-from-fn-call-is-fine-test
   (is (match? []
               (lint "(defn make-some-opts [] {})"
                     "(fs/with-temp-dir [bname (make-some-opts)]"
                     "  (spit bname \"foo\"))"))))
 
-(deftest with-temp-dir-options-type-is-not-validated
+(deftest with-temp-dir-options-type-is-not-validated-test
   ;; our linter does not catch clearly invalid option types, for example:
   (is (match? []
               (lint "(fs/with-temp-dir [bname :bad-options]"
