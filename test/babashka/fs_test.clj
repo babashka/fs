fda1ff2aa5b6e124686f614fb0c74ee79c8ae522
Lee Read <lee@dlread.com>
2025-11-23 10:52:32 +0000

    test & ci: explicitly support clojure >= 1.10.3 (#173)
    Run `test`, `test-cwd` or `test-all` tasks with `:clj-1.10`, `:clj-1.11`,
    `:clj-1.12` (default) or `:clj-all`.
    
    CI now runs `test-all :clj-all`.
    
    Mention min clojure version in README.
    
    Adjustments:
    - always delete-on-exit for (gensym) based files, I found they reoccurred
    when switching Clojure versions and caused tests to fail.
    - adjust test that used `to-long` which is not available in clojure 1.10
    
    Closes #168

--- test/babashka/fs_test.clj
+++ test/babashka/fs_test.clj
@@ -810,7 +810,8 @@
     (is (= (fs/exists? "c:/123:456") false))))
 
 (deftest write-bytes-test
-  (let [f (fs/path (fs/temp-dir) (str (gensym)))]
+  (let [f (-> (fs/path (fs/temp-dir) (str (gensym)))
+              fs/delete-on-exit)]
     (fs/write-bytes f (.getBytes (String. "foo")))
     (is (= "foo" (String. (fs/read-all-bytes f))))
     ;; again, truncation behavior:
@@ -820,7 +821,8 @@
     (is (= "foobar" (String. (fs/read-all-bytes f))))))
 
 (deftest write-lines-test
-  (let [f (fs/path (fs/temp-dir) (str (gensym)))]
+  (let [f (-> (fs/path (fs/temp-dir) (str (gensym)))
+              fs/delete-on-exit)]
     (fs/write-lines f (repeat 3 "foo"))
     (is (= (repeat 3 "foo") (fs/read-all-lines f)))
     ;; again, truncation behavior:
@@ -844,11 +846,14 @@
     (let [new-val (fs/update-file file str (rand))]
       (is (= new-val (slurp file)))))
 
-  (let [file (fs/file (fs/temp-dir) (str (gensym)))]
+  (let [file (-> (fs/file (fs/temp-dir) (str (gensym)))
+                 fs/delete-on-exit)]
     (spit file ", ")
     (is (= "foo, bar, baz" (fs/update-file file str/join ["foo" "bar" "baz"]))))
 
-  (let [path (fs/path (fs/file (fs/temp-dir) (str (gensym))))]
+  (let [path (-> (fs/file (fs/temp-dir) (str (gensym)))
+                 fs/delete-on-exit
+                 fs/path)]
     (spit (fs/file path) "foo")
     (is (= "foobar" (fs/update-file path str "bar")))))
 
